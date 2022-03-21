20f2aad79e1dc5dee37ade26a6020b7cfbe75be7
Ryan Mwaniki <ryanmwaniki60@gmail.com>
2022-03-21 16:46:58 +0300

    Initial Commit
    First Commit

---
+++ WindowsApp1/LandingPage.vb
@@ -0,0 +1,10 @@
+﻿Public Class LandingPage
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmdSignIn.Click
+        Registration.Show()
+    End Sub
+
+    Private Sub cmdSignUp_Click(sender As Object, e As EventArgs) Handles cmdSignUp.Click
+        Registration.Show()
+
+    End Sub
+End Class
